e6c153fca50c7ccf5858d819c7e101a11747b035
m <m@bitsnbites.eu>
2016-09-30 19:24:47 +0200

    Fix header

--- src/lib/hzr_crc32.h
+++ src/lib/hzr_crc32.h
@@ -4,6 +4,14 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 uint32_t _hzr_crc32(const void* data, size_t length);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* HZR_CRC32_H_ */
