ebdabcae796f00e604c952ab1fa494148e301c6d
m <m@bitsnbites.eu>
2016-10-14 23:14:17 +0200

    Turn ReadStream into a C-style struct

--- src/lib/hzr_decode.cpp
+++ src/lib/hzr_decode.cpp
@@ -6,104 +6,56 @@
 #include "hzr_crc32c.h"
 #include "hzr_internal.h"
 
-namespace hzr {
-
-namespace {
-
-// A class to help decoding binary data.
-class ReadStream {
- public:
-  // Initialize a bitstream.
-  ReadStream(const void* buf, int size);
-
-  // Copy constructor.
-  ReadStream(const ReadStream& other);
-
-  // Read one bit from a bitstream.
-  int ReadBit();
-
-  // Read one bit from a bitstream, with checking.
-  int ReadBitChecked();
-
-  // Read bits from a bitstream.
-  uint32_t ReadBits(int bits);
-
-  // Read bits from a bitstream, with checking.
-  uint32_t ReadBitsChecked(int bits);
-
-  // Peek eight bits from a bitstream (read without advancing the pointer).
-  uint8_t Peek8Bits() const;
-
-  // Read 16 bits from a bitstream, byte aligned.
-  uint32_t Read16BitsAligned();
-
-  // Align the stream to a byte boundary (do nothing if already aligned).
-  void AlignToByte();
-
-  // Advance the pointer by N bits.
-  void Advance(int N);
-
-  // Advance N bytes.
-  void AdvanceBytes(int N);
-
-  // Advance N bytes, with checking.
-  void AdvanceBytesChecked(int N);
-
-  // Check if we have reached the end of the buffer.
-  bool AtTheEnd() const;
-
-  const uint8_t* byte_ptr() const { return byte_ptr_; }
-
-  // Check if any of the Read*Checked() methods failed.
-  bool read_failed() const { return read_failed_; }
-
- private:
-  const uint8_t* byte_ptr_;
-  int bit_pos_;
-  const uint8_t* end_ptr_;
-  bool read_failed_;
+// A helper for decoding binary data.
+struct ReadStream {
+  const uint8_t* byte_ptr;
+  int bit_pos;
+  const uint8_t* end_ptr;
+  bool read_failed;
 };
 
-ReadStream::ReadStream(const void* buf, int size)
-    : byte_ptr_(reinterpret_cast<const uint8_t*>(buf)),
-      bit_pos_(0),
-      end_ptr_(reinterpret_cast<const uint8_t*>(buf) + size),
-      read_failed_(false) {}
+// Check if we have reached the end of the buffer.
+static bool AtTheEnd(const ReadStream* stream);
 
-ReadStream::ReadStream(const ReadStream& other)
-    : byte_ptr_(other.byte_ptr_),
-      bit_pos_(other.bit_pos_),
-      end_ptr_(other.end_ptr_),
-      read_failed_(other.read_failed_) {}
+// Initialize a bitstream.
+static void InitReadStream(ReadStream* stream, const void* buf, int size) {
+  stream->byte_ptr = (const uint8_t*)buf;
+  stream->bit_pos = 0;
+  stream->end_ptr = ((const uint8_t*)buf) + size;
+  stream->read_failed = false;
+}
 
-int ReadStream::ReadBit() {
+// Read one bit from a bitstream.
+static int ReadBit(ReadStream* stream) {
   // Extract one bit.
-  int x = (*byte_ptr_ >> bit_pos_) & 1;
-  bit_pos_ = (bit_pos_ + 1) & 7;
-  if (!bit_pos_) {
-    ++byte_ptr_;
+  int x = (*stream->byte_ptr >> stream->bit_pos) & 1;
+  stream->bit_pos = (stream->bit_pos + 1) & 7;
+  if (stream->bit_pos == 0) {
+    ++stream->byte_ptr;
   }
 
   return x;
 }
 
-int ReadStream::ReadBitChecked() {
+// Read one bit from a bitstream, with checking.
+static int ReadBitChecked(ReadStream* stream) {
   // Check that we don't read past the end.
-  if (UNLIKELY(byte_ptr_ >= end_ptr_)) {
-    read_failed_ = true;
+  if (UNLIKELY(stream->byte_ptr >= stream->end_ptr)) {
+    stream->read_failed = true;
     return 0;
   }
 
   // Ok, read...
-  return ReadBit();
+  return ReadBit(stream);
 }
 
-uint32_t ReadStream::ReadBits(int bits) {
+// Read multiple bits from a bitstream.
+static uint32_t ReadBits(ReadStream* stream, int bits) {
   uint32_t x = 0;
 
   // Get current stream state.
-  const uint8_t* buf = byte_ptr_;
-  int bit = bit_pos_;
+  const uint8_t* buf = stream->byte_ptr;
+  int bit = stream->bit_pos;
 
   // Extract bits.
   // TODO(m): Optimize this!
@@ -124,76 +76,66 @@ uint32_t ReadStream::ReadBits(int bits) {
   }
 
   // Store new stream state.
-  bit_pos_ = bit;
-  byte_ptr_ = buf;
+  stream->bit_pos = bit;
+  stream->byte_ptr = buf;
 
   return x;
 }
 
-uint32_t ReadStream::ReadBitsChecked(int bits) {
+// Read multiple bits from a bitstream, with checking.
+static uint32_t ReadBitsChecked(ReadStream* stream, int bits) {
   // Check that we don't read past the end.
-  int new_bit_pos = bit_pos_ + bits;
-  const uint8_t* new_byte_ptr = byte_ptr_ + (new_bit_pos >> 3);
-  if (UNLIKELY(new_byte_ptr > end_ptr_ ||
-               (new_byte_ptr == end_ptr_ && ((new_bit_pos & 7) != 0)))) {
-    read_failed_ = true;
+  int new_bit_pos = stream->bit_pos + bits;
+  const uint8_t* new_byte_ptr = stream->byte_ptr + (new_bit_pos >> 3);
+  if (UNLIKELY(new_byte_ptr > stream->end_ptr ||
+               (new_byte_ptr == stream->end_ptr && ((new_bit_pos & 7) != 0)))) {
+    stream->read_failed = true;
     return 0;
   }
 
   // Ok, read...
-  return ReadBits(bits);
+  return ReadBits(stream, bits);
 }
 
-uint8_t ReadStream::Peek8Bits() const {
-  uint8_t lo = byte_ptr_[0], hi = byte_ptr_[1];
-  return ((hi << 8) | lo) >> bit_pos_;
+// Peek eight bits from a bitstream (read without advancing the pointer).
+static uint8_t Peek8Bits(const ReadStream* stream) {
+  uint8_t lo = stream->byte_ptr[0], hi = stream->byte_ptr[1];
+  return ((hi << 8) | lo) >> stream->bit_pos;
 }
 
-uint32_t ReadStream::Read16BitsAligned() {
-  // TODO(m): Check that we don't read past the end.
-
-  AlignToByte();
-  uint32_t lo = byte_ptr_[0], hi = byte_ptr_[1];
-  byte_ptr_ += 2;
-  return (hi << 8) | lo;
+// Advance the pointer by N bits.
+static void Advance(ReadStream* stream, int N) {
+  int new_bit_pos = stream->bit_pos + N;
+  stream->bit_pos = new_bit_pos & 7;
+  stream->byte_ptr += new_bit_pos >> 3;
 }
 
-void ReadStream::AlignToByte() {
-  if (LIKELY(bit_pos_)) {
-    bit_pos_ = 0;
-    ++byte_ptr_;
-  }
-}
-void ReadStream::Advance(int N) {
-  int new_bit_pos = bit_pos_ + N;
-  bit_pos_ = new_bit_pos & 7;
-  byte_ptr_ += new_bit_pos >> 3;
-}
-
-void ReadStream::AdvanceBytes(int N) {
-  byte_ptr_ += N;
-}
-
-void ReadStream::AdvanceBytesChecked(int N) {
-  const uint8_t* new_byte_ptr = byte_ptr_ + N;
+// Advance N bytes, with checking.
+static void AdvanceBytesChecked(ReadStream* stream, int N) {
+  const uint8_t* new_byte_ptr = stream->byte_ptr + N;
 
   // Check that we don't advance past the end.
-  if (UNLIKELY(new_byte_ptr > end_ptr_ ||
-               (new_byte_ptr == end_ptr_ && (bit_pos_ != 0)))) {
-    read_failed_ = true;
+  if (UNLIKELY(new_byte_ptr > stream->end_ptr ||
+               (new_byte_ptr == stream->end_ptr && (stream->bit_pos != 0)))) {
+    stream->read_failed = true;
     return;
   }
 
   // Ok, advance...
-  byte_ptr_ = new_byte_ptr;
+  stream->byte_ptr = new_byte_ptr;
 }
 
-bool ReadStream::AtTheEnd() const {
+// Check if we have reached the end of the buffer.
+static bool AtTheEnd(const ReadStream* stream) {
   // This is a rought estimate that we have reached the end of the input
   // buffer (not too short, and not too far).
-  return (byte_ptr_ == end_ptr_ && bit_pos_ == 0) ||
-         (byte_ptr_ == (end_ptr_ - 1) && bit_pos_ > 0);
+  return (stream->byte_ptr == stream->end_ptr && stream->bit_pos == 0) ||
+         (stream->byte_ptr == (stream->end_ptr - 1) && stream->bit_pos > 0);
 }
+
+namespace hzr {
+
+namespace {
 
 struct DecodeNode {
   DecodeNode *child_a, *child_b;
@@ -210,7 +152,7 @@ struct DecodeTree {
   DecodeNode* Recover(int* node_num,
                       uint32_t code,
                       int bits,
-                      ReadStream& stream);
+                      ReadStream* stream);
 
   DecodeNode nodes[kMaxTreeNodes];
   DecodeLutEntry decode_lut[256];
@@ -220,7 +162,7 @@ struct DecodeTree {
 DecodeNode* DecodeTree::Recover(int* node_num,
                                 uint32_t code,
                                 int bits,
-                                ReadStream& stream) {
+                                ReadStream* stream) {
   // Pick a node from the node array.
   DecodeNode* this_node = &nodes[*node_num];
   *node_num = *node_num + 1;
@@ -234,14 +176,14 @@ DecodeNode* DecodeTree::Recover(int* node_num,
   this_node->child_b = nullptr;
 
   // Is this a leaf node?
-  bool is_leaf = stream.ReadBitChecked() != 0;
-  if (UNLIKELY(stream.read_failed())) {
+  bool is_leaf = ReadBitChecked(stream) != 0;
+  if (UNLIKELY(stream->read_failed)) {
     return nullptr;
   }
   if (is_leaf) {
     // Get symbol from tree description and store in lead node.
-    int symbol = static_cast<int>(stream.ReadBitsChecked(kSymbolSize));
-    if (UNLIKELY(stream.read_failed())) {
+    int symbol = static_cast<int>(ReadBitsChecked(stream, kSymbolSize));
+    if (UNLIKELY(stream->read_failed)) {
       return nullptr;
     }
 
@@ -301,17 +243,18 @@ extern "C" hzr_status_t hzr_verify(const void* in,
   }
 
   // Parse the header.
-  hzr::ReadStream stream(in, in_size);
-  *decoded_size = static_cast<size_t>(stream.ReadBitsChecked(32));
-  uint32_t expected_crc32 = stream.ReadBitsChecked(32);
-  if (stream.read_failed()) {
+  ReadStream stream;
+  InitReadStream(&stream, in, in_size);
+  *decoded_size = static_cast<size_t>(ReadBitsChecked(&stream, 32));
+  uint32_t expected_crc32 = ReadBitsChecked(&stream, 32);
+  if (stream.read_failed) {
     DBREAK("Could not read the header.");
     return HZR_FAIL;
   }
 
   // Check the checksum.
   uint32_t actual_crc32 =
-      _hzr_crc32(stream.byte_ptr(), in_size - HZR_HEADER_SIZE);
+      _hzr_crc32(stream.byte_ptr, in_size - HZR_HEADER_SIZE);
   if (actual_crc32 != expected_crc32) {
     DBREAK("CRC32 check failed.");
     return HZR_FAIL;
@@ -336,9 +279,10 @@ extern "C" hzr_status_t hzr_decode(const void* in,
   }
 
   // Skip the header.
-  hzr::ReadStream stream(in, in_size);
-  stream.AdvanceBytesChecked(HZR_HEADER_SIZE);
-  if (stream.read_failed()) {
+  ReadStream stream;
+  InitReadStream(&stream, in, in_size);
+  AdvanceBytesChecked(&stream, HZR_HEADER_SIZE);
+  if (stream.read_failed) {
     DBREAK("Unable to skip past the header.");
     return HZR_FAIL;
   }
@@ -346,7 +290,7 @@ extern "C" hzr_status_t hzr_decode(const void* in,
   // Recover the Huffman tree.
   hzr::DecodeTree tree;
   int node_count = 0;
-  hzr::DecodeNode* tree_root = tree.Recover(&node_count, 0, 0, stream);
+  hzr::DecodeNode* tree_root = tree.Recover(&node_count, 0, 0, &stream);
   if (tree_root == nullptr) {
     DBREAK("Unable to decode the Huffman tree.");
     return HZR_FAIL;
@@ -367,8 +311,8 @@ extern "C" hzr_status_t hzr_decode(const void* in,
     // Peek 8 bits from the stream and use it to look up a potential symbol in
     // the LUT (codes that are eight bits or shorter are very common, so we have
     // a high hit rate in the LUT).
-    const auto& lut_entry = tree.decode_lut[stream.Peek8Bits()];
-    stream.Advance(lut_entry.bits);
+    const auto& lut_entry = tree.decode_lut[Peek8Bits(&stream)];
+    Advance(&stream, lut_entry.bits);
     if (LIKELY(lut_entry.node == nullptr)) {
       // Fast case: We found the symbol in the LUT.
       symbol = lut_entry.symbol;
@@ -378,7 +322,7 @@ extern "C" hzr_status_t hzr_decode(const void* in,
       hzr::DecodeNode* node = lut_entry.node;
       while (node->symbol < 0) {
         // Get next node.
-        if (stream.ReadBit()) {
+        if (ReadBit(&stream)) {
           node = node->child_b;
         } else {
           node = node->child_a;
@@ -400,19 +344,19 @@ extern "C" hzr_status_t hzr_decode(const void* in,
           break;
         }
         case kSymUpTo6Zeros: {
-          zero_count = static_cast<int>(stream.ReadBits(2)) + 3;
+          zero_count = static_cast<int>(ReadBits(&stream, 2)) + 3;
           break;
         }
         case kSymUpTo22Zeros: {
-          zero_count = static_cast<int>(stream.ReadBits(4)) + 7;
+          zero_count = static_cast<int>(ReadBits(&stream, 4)) + 7;
           break;
         }
         case kSymUpTo278Zeros: {
-          zero_count = static_cast<int>(stream.ReadBits(8)) + 23;
+          zero_count = static_cast<int>(ReadBits(&stream, 8)) + 23;
           break;
         }
         case kSymUpTo16662Zeros: {
-          zero_count = static_cast<int>(stream.ReadBits(14)) + 279;
+          zero_count = static_cast<int>(ReadBits(&stream, 14)) + 279;
           break;
         }
         default: {
@@ -438,18 +382,18 @@ extern "C" hzr_status_t hzr_decode(const void* in,
     // Special case: Only one symbol in the entire tree -> root node is a leaf
     // node.
     if (node->symbol >= 0) {
-      stream.Advance(1);
+      Advance(&stream, 1);
     }
 
     while (node->symbol < 0) {
       // Get next node.
-      if (stream.ReadBitChecked()) {
+      if (ReadBitChecked(&stream)) {
         node = node->child_b;
       } else {
         node = node->child_a;
       }
 
-      if (UNLIKELY(stream.read_failed())) {
+      if (UNLIKELY(stream.read_failed)) {
         DBREAK("Input buffer ended prematurely.");
         return HZR_FAIL;
       }
@@ -469,19 +413,19 @@ extern "C" hzr_status_t hzr_decode(const void* in,
           break;
         }
         case kSymUpTo6Zeros: {
-          zero_count = static_cast<int>(stream.ReadBitsChecked(2)) + 3;
+          zero_count = static_cast<int>(ReadBitsChecked(&stream, 2)) + 3;
           break;
         }
         case kSymUpTo22Zeros: {
-          zero_count = static_cast<int>(stream.ReadBitsChecked(4)) + 7;
+          zero_count = static_cast<int>(ReadBitsChecked(&stream, 4)) + 7;
           break;
         }
         case kSymUpTo278Zeros: {
-          zero_count = static_cast<int>(stream.ReadBitsChecked(8)) + 23;
+          zero_count = static_cast<int>(ReadBitsChecked(&stream, 8)) + 23;
           break;
         }
         case kSymUpTo16662Zeros: {
-          zero_count = static_cast<int>(stream.ReadBitsChecked(14)) + 279;
+          zero_count = static_cast<int>(ReadBitsChecked(&stream, 14)) + 279;
           break;
         }
         default: {
@@ -490,7 +434,7 @@ extern "C" hzr_status_t hzr_decode(const void* in,
         }
       }
 
-      if (UNLIKELY(stream.read_failed() || out_ptr + zero_count > out_end)) {
+      if (UNLIKELY(stream.read_failed || out_ptr + zero_count > out_end)) {
         DBREAK("Output buffer full.");
         return HZR_FAIL;
       }
@@ -500,7 +444,7 @@ extern "C" hzr_status_t hzr_decode(const void* in,
   }
 
   // TODO: Better check!
-  if (UNLIKELY(!stream.AtTheEnd())) {
+  if (UNLIKELY(!AtTheEnd(&stream))) {
     DBREAK("Decoder did not reach the end of the input buffer.");
     return HZR_FAIL;
   }
