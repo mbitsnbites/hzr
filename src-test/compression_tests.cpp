7e62202d8b33e92c4c495dc3c59ccc8a7be66714
m <m@bitsnbites.eu>
2016-11-11 21:27:07 +0100

    Better test code structure

--- src-test/compression_tests.cpp
+++ src-test/compression_tests.cpp
@@ -20,7 +20,6 @@
 //  3. This notice may not be removed or altered from any source distribution.
 //------------------------------------------------------------------------------
 
-#define CATCH_CONFIG_MAIN
 #include <catch.hpp>
 
 #include <stdio.h>
